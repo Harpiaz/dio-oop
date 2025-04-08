5b6a9e3a4891f3c474659eb2e2d4dfaab4bf9453
Harpiaz <matffc@hotmail.com>
2025-04-08 19:17:57 -0300

    Implementação das interfaces e da classe IPhone

--- dio-oop/src/IPhone.java
+++ dio-oop/src/IPhone.java
@@ -1,3 +1,39 @@
-public class IPhone {
+public class IPhone implements ReprodutorMusical, AparelhoTelefonico, NavegadorNaInternet {
 
+    public void ligar(String numero) {
+        System.out.println("Ligando para " + numero);
+    }
+
+    public void atender() {
+        System.out.println("Atendendo chamada");
+    }
+
+    public void iniciarCorreioVoz() {
+        System.out.println("Iniciando correio de voz...");
+    }
+
+    public void exibirPagina(String url) {
+        System.out.println("Exibindo página: " + url);
+    }
+
+    public void adicionarNovaAba() {
+        System.out.println("Adicionando nova aba...");
+    }
+
+    public void atualizarPagina() {
+        System.out.println("Atualizando página");
+    }
+
+    public void tocar() {
+        System.out.println("Tocando música");
+    }
+
+    public void pausar() {
+        System.out.println("Pausando música");
+    }
+
+    public void selecionarMusica(String musica) {
+        System.out.println("Selecionando a música " + musica);
+
+    }
 }
--- dio-oop/src/NavegadorNaInternet.java
+++ dio-oop/src/NavegadorNaInternet.java
@@ -1,3 +1,5 @@
 public interface NavegadorNaInternet {
-
+    public void exibirPagina(String url);
+    public void adicionarNovaAba();
+    public void atualizarPagina();
 }
